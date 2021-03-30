202628cf41f0f3484ed35ed1f8cf92107ad6ea13
Sri <srivathsan@oasys.co>
2021-03-30 12:32:51 +0530

    3/30

--- app/src/main/java/com/hmis_tn/doctor/utils/utils.kt
+++ app/src/main/java/com/hmis_tn/doctor/utils/utils.kt
@@ -484,6 +484,17 @@ class Utils(val context: Context) {
             .displayMetrics.densityDpi.toFloat() / DisplayMetrics.DENSITY_DEFAULT)
     }
 
+    /**
+     * This method converts device specific pixels to density independent pixels.
+     *
+     * @param px A value in px (pixels) unit. Which we need to convert into db
+     * @param context Context to get resources and device specific display metrics
+     * @return A float value to represent dp equivalent to px value
+     */
+    fun convertPixelsToDp(px: Float, context: Context): Float {
+        return px / (context.resources
+            .displayMetrics.densityDpi.toFloat() / DisplayMetrics.DENSITY_DEFAULT)
+    }
 
     @SuppressLint("ObsoleteSdkInt")
     fun getLocaleStringResource(
@@ -511,18 +522,6 @@ class Utils(val context: Context) {
             resources.updateConfiguration(conf, null)
         }
         return result
-    }
-
-    /**
-     * This method converts device specific pixels to density independent pixels.
-     *
-     * @param px A value in px (pixels) unit. Which we need to convert into db
-     * @param context Context to get resources and device specific display metrics
-     * @return A float value to represent dp equivalent to px value
-     */
-    fun convertPixelsToDp(px: Float, context: Context): Float {
-        return px / (context.resources
-            .displayMetrics.densityDpi.toFloat() / DisplayMetrics.DENSITY_DEFAULT)
     }
 
     fun getDate(ourDate: String): String? {
